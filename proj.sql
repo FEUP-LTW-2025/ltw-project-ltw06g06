0887f1f851e458c2768598b7a781d85d572adfcc
zelobo009 <zelobo@DESKTOP-PRQTS52>
2025-04-03 14:22:27 +0100

    Database scrpit

--- proj.sql
+++ proj.sql
@@ -1,6 +1,32 @@
 CREATE TABLE USER {
-    id INTEGER PRIMARY KEY;
-    username VARCHAR NOT NULL;
-    password VARCHAR NOT NULL;
-    profileP BLOB NOT NULL;
-}
+    id INTEGER PRIMARY KEY,
+    username VARCHAR NOT NULL,
+    password VARCHAR NOT NULL,
+    email VARCHAR NOT NULL,
+    profileP VARCHAR
+};
+
+CREATE TABLE Client {
+    id INTEGER PRIMARY KEY, 
+    isAdmin BOOLEAN,
+    FOREIGN KEY (id) REFERENCES USER(id)
+};
+
+CREATE TABLE Artist {
+    id INTEGER PRIMARY KEY, 
+    category VARCHAR NOT NULL,
+    RATING REAL NOT NULL,
+    description text,
+    avgTime REAL NOT NULL,
+    FOREIGN KEY (id) REFERENCES USER(id)
+};
+
+CREATE TABLE Service {
+    id INTEGER PRIMARY KEY,
+    Sname VARCHAR NOT NULL,
+    category VARCHAR NOT NULL,
+    RATING REAL NOT NULL,
+    description text,
+    avgTime REAL NOT NULL,
+    FOREIGN KEY (id) REFERENCES USER(id)
+};
