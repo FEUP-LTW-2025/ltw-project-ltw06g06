d66393988e62c690cd29c35721614479648fb3c3
Zelobo009 <ze.gama.lobo@gmail.com>
2025-05-14 17:34:03 +0100

    Fix

--- actions/action_login.php
+++ actions/action_login.php
@@ -7,14 +7,15 @@
 
     $db = getDatabase();
 
-
+    
     if (userExists($db, $_POST['username'], $_POST['password'])){
         $_SESSION['username'] = $_POST['username'];
+        $_SESSION['crsx'] = "placeholder";
         header('Location: ../index.php' ); 
 
     }
     else{   
-        var_dump(password_hash($_POST['password'],PASSWORD_DEFAULT));
+        header('Location: ../login.php');
     }
 
 ?>
