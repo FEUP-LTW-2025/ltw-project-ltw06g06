40313c8c3e122a7b3568f2c075a7580fd3530372
Zelobo009 <ze.gama.lobo@gmail.com>
2025-05-24 17:16:03 +0100

    Code organization

--- pages/become_artist.php
+++ pages/become_artist.php
@@ -9,43 +9,7 @@
   $categories = getCategories($db);
 
   drawMainHeader($categories);
+  drawBecomeArtists($categories);
+  drawFooter();
+
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Artist Registration</title>
-</head>
-<body>
-  <section class="service-request-form">
-  <h2>Become an Artist</h2>
-
-  <form action="../actions/action_become_artist.php" method="post">
-    
-    <div class="form-group">
-      <label for="bio">Brief Description of Yourself:</label>
-      <textarea name="bio" id="bio" rows="5" required placeholder="Tell us a bit about you, your skills, and what you offer."></textarea>
-    </div>
-
-    <div class="form-group">
-      <label for="category">Category:</label>
-      <select name="category" id="category" required>
-      <option value="">Select a Category </option>
-      <?php foreach ($categories as $cat) {
-        $id = $cat['id'] ?? '';
-        $name = $cat['name'] ?? 'Unnamed';
-      ?>
-        <option value="<?= htmlspecialchars((string)$cat['name']) ?>">
-          <?= htmlspecialchars((string)$cat['name']) ?>
-        </option>
-      <?php } ?>
-    </select>
-    </div>
-
-    <div class="form-group">
-      <button type="submit">Submit</button>
-    </div>
-
-  </form>
-</section>
-</body>
-</html>
